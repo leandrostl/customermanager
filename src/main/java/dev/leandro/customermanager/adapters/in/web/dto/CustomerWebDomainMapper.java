5ad90b738d5f13760fb32a7d79a9c1e48e6a1c79
leandrostl <leandro.stlima@gmail.com>
2024-09-28 15:38:03 -0300

    Passa a permitir criar customers

--- src/main/java/dev/leandro/customermanager/adapters/in/web/dto/CustomerWebDomainMapper.java
+++ src/main/java/dev/leandro/customermanager/adapters/in/web/dto/CustomerWebDomainMapper.java
@@ -3,15 +3,13 @@ package dev.leandro.customermanager.adapters.in.web.dto;
 import dev.leandro.customermanager.application.domain.model.Customer;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface CustomerWebDomainMapper {
-    CustomerWebDomainMapper INSTANCE = Mappers.getMapper(CustomerWebDomainMapper.class);
-
     @Mapping(source = "id.value", target = "id")
     CustomerResponse toResponse(Customer customer);
 
+    @Mapping(target = "id", ignore = true)
     @Mapping(source = "document", target = "document.code")
     @Mapping(source = "documentType", target = "document.type")
     Customer toDomainModel(CustomerRequest customerRequest);
